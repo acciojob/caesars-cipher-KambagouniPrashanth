241cda7eb0e75b5cae802cce41930e7afdbf6e14
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-23 21:35:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const lookup = {
   'A': 'N','B': 'O','C': 'P','D': 'Q',
   'E': 'R','F': 'S','G': 'T','H': 'U',
@@ -7,19 +8,26 @@ const lookup = {
   'U': 'H','V': 'I','W': 'J','X': 'K',
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
-
-function rot13(encodedStr){
-   let decodedArr = []; 
-	for(let i=0; i<encodedStr.length; i++){
-		if(lookup[encodedStr[i]]===undefined){
-			decodedArr.push(encodedStr[i]);
-		}else{
-			decodedArr.push(lookup[encodedStr[i]);
+function rot13(str) {
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+	for(let i=0;i<str.length;i++){
+		if(lookup[str[i]]===undefined){
+			decodedArr.push(str[i]);
 		}
+		else{
+			decodedArr.push(lookup[str[i]]);
+		}
+		
+		
 	}
-  return decodedArr;
+
+  return decodedArr; //return decodedArr
 }
-console.log(rot13("#AB"));
 
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-module.exports = rot13;
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
+
+// Do not change this line
+window.rot13 = rot13
